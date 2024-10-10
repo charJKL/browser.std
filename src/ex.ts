737d6440ed1ae543ebe1381a6d291f0cd40d695b
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-10 18:55:54 +0200

    Implement `isString` function.

--- src/ex.ts
+++ src/ex.ts
@@ -2,4 +2,5 @@ export { ArrayEx } from "./ex/ArrayEx";
 export { isError } from "./ex/isError.func";
 export { isFunction } from "./ex/isFunction.func";
 export { isNull, isNotNull } from "./ex/isNull.func";
+export { isString, isNotString } from "./ex/isString.func";
 export { isUndefined, isNotUndefined } from "./ex/isUndefined.func";

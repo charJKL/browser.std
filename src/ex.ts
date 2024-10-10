43d1d8a5488b9367fe70d98bc5b3a87b084d1c3d
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-10 18:55:06 +0200

    Implement `isFunction` function.

--- src/ex.ts
+++ src/ex.ts
@@ -1,3 +1,4 @@
 export { ArrayEx } from "./ex/ArrayEx";
 export { isError } from "./ex/isError.func";
+export { isFunction } from "./ex/isFunction.func";
 export { isUndefined, isNotUndefined } from "./ex/isUndefined.func";

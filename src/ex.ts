c4b5f22e02520df97f78559889d2e42d256e9d9e
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-10 18:55:35 +0200

    Implement `isNull` function.

--- src/ex.ts
+++ src/ex.ts
@@ -1,4 +1,5 @@
 export { ArrayEx } from "./ex/ArrayEx";
 export { isError } from "./ex/isError.func";
 export { isFunction } from "./ex/isFunction.func";
+export { isNull, isNotNull } from "./ex/isNull.func";
 export { isUndefined, isNotUndefined } from "./ex/isUndefined.func";

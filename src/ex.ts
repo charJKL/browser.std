77143165c847d8989e2113cf66f3971d1800bedc
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-16 20:17:23 +0200

    Export `IsObjct`, `StdError`.

--- src/ex.ts
+++ src/ex.ts
@@ -2,5 +2,8 @@ export { ArrayEx } from "./ex/ArrayEx";
 export { isError } from "./ex/isError.func";
 export { isFunction } from "./ex/isFunction.func";
 export { isNull, isNotNull } from "./ex/isNull.func";
+export { isObject, isNotObject } from "./ex/isObject.func";
 export { isString, isNotString } from "./ex/isString.func";
 export { isUndefined, isNotUndefined } from "./ex/isUndefined.func";
+
+export { StdError }  from "./ex/StdError";

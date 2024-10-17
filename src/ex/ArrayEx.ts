3c8a68f78ee2fa3cbac8a8f2fda2a545e71c39dc
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-17 15:13:21 +0200

    Implement `ArrayEx.AsyncMap` method.

--- src/ex/ArrayEx.ts
+++ src/ex/ArrayEx.ts
@@ -11,4 +11,10 @@ export class ArrayEx // eslint-disable-line @typescript-eslint/no-extraneous-cla
 		if(ArrayEx.IsEmpty(array)) return array;
 		return array.sort((a, b) => a - b);
 	}
+	
+	public static AsyncMap<T, V>(array: Array<T>, callbackfn: (value: T, index: number, array: Array<T>) => Promise<V>, thisArg?: unknown) : Promise<PromiseSettledResult<V>[]>
+	{
+		const result = array.map(callbackfn, thisArg);
+		return Promise.allSettled(result);
+	}
 }

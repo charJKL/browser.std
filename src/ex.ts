37882ef81c73acebc8e1dbb4a3720049d0d8c964
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-17 08:11:02 +0200

    Export `hasProp` function.

--- src/ex.ts
+++ src/ex.ts
@@ -1,9 +1,11 @@
 export { ArrayEx } from "./ex/ArrayEx";
+export { hasProp } from "./ex/hasProp.func";
 export { isError } from "./ex/isError.func";
 export { isFunction } from "./ex/isFunction.func";
 export { isNull, isNotNull } from "./ex/isNull.func";
 export { isObject, isNotObject } from "./ex/isObject.func";
 export { isString, isNotString } from "./ex/isString.func";
 export { isUndefined, isNotUndefined } from "./ex/isUndefined.func";
+
 
 export { StdError }  from "./ex/StdError";

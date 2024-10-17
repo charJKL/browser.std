e9bac1f0fae7131e5d2e01e7df7fa4ebf3184d80
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-17 15:16:22 +0200

    Implement `sendNotification` method.

--- src/api/backend/BackendComm.ts
+++ src/api/backend/BackendComm.ts
@@ -3,20 +3,23 @@ import type { MessageBlueprintParametered, MessageBlueprint, SupportedMessages, 
 import type { NotificationData, SupportedNotifications } from "../CommProtocol";
 import type { MessageSender, SendResponse, Variants } from "../CommProtocol";
 import { BrowserApiError } from "../BrowserApiError";
+import { ArrayEx, isError, isUndefined } from "../../ex";
 import { ApiReturn } from "../ApiReturn.type";
+
+type BrowserTab = browser.tabs.Tab;
 type AllowListenerBeAsync<T> = Promise<T> | T;
 type MessageArgs<B extends MessageBlueprint> = B extends MessageBlueprintParametered ? Parameters<B>[0] : undefined;
 type MessageCommArgs<B extends MessageBlueprint> = B extends MessageBlueprintParametered ? {sender: MessageSender} & MessageArgs<B> : {sender: MessageSender};
 type MessageCommReturn<B extends MessageBlueprint> = AllowListenerBeAsync<ReturnType<B>>;
 type MessageCommListener<B extends MessageBlueprint> = (args: MessageCommArgs<B>) => MessageCommReturn<B>;
 
-type NotificationData<B extends NotifiactionBlueprint> = ReturnType<B>;
-
-type Variants<L> = keyof L & string;
-
+// #region errors
+class TabsApiCallError extends BrowserApiError<"TabsApiCallError",  BackendComm<any, any>, {tabUrl: string} | {tab: BrowserTab, packet: PacketResponse}>{ };
+class NoTabsWasFound extends BrowserApiError<"NoTabsWasFound", BackendComm<any, any>, {url: string}>{ };
+class SendWasntSuccessfulError extends BrowserApiError<"SendWasntSuccessfulError", BackendComm<any, any>, {results: unknown[]}>{ };
 class InvalidPacketError extends BrowserApiError<"InvalidPacketDescError", BackendComm<any, any>, {rawPacket: unknown, sender: MessageSender}>{ }; // TODO what to do with any? 
 class MissingListenerError extends BrowserApiError<"MissingListnerError", BackendComm<any, any>, {packet: Packet, sender: MessageSender}>{ }; // TODO what to do with any?
-
+// #endregion
 
 export class BackendComm<SM extends SupportedMessages, SN extends SupportedNotifications>
 {
@@ -33,9 +36,28 @@ export class BackendComm<SM extends SupportedMessages, SN extends SupportedNotif
 		this.listeners.set(variant, listeners);
 	}
 	
-	public sendNotification<V extends Variants<SN>>(tabUrl: string, variant: V, data: NotificationData<SN[V]>)
+	public async sendNotification<V extends Variants<SN>>(tabUrl: string, variant: V, data: NotificationData<SN[V]>) : ApiReturn<boolean, TabsApiCallError, NoTabsWasFound, SendWasntSuccessfulError>
 	{
-		console.log("Send notification", "tabUrl=", tabUrl, "variant=", variant, "args=", data); // TODO implement sending notifications
+		console.log("Send notification", "tabUrl=", tabUrl, "variant=", variant, "args=", data); // TODO delete this, for debug purpose only
+		const returnError = (reason: unknown) => new TabsApiCallError(this,  "Internal browser call `tabs.query()` throw error.", {tabUrl}, reason);
+		const tabs = await browser.tabs.query({url: tabUrl}).catch(returnError); // TODO should `BackendComm` directly use `browser.tabs`? or had to inject `BrowserTabs`?
+		if(isError(TabsApiCallError, tabs)) return tabs;
+		if(ArrayEx.IsEmpty(tabs)) return new NoTabsWasFound(this, "Wanted url is not opened in any tab.", {url: tabUrl});
+		const results = await ArrayEx.AsyncMap(tabs, tab => this.dispatchNotification.call(this, tab, variant, data));
+		const wasErrorOccuredDuringDispatchingNotifications = (result: PromiseSettledResult<boolean | TabsApiCallError>) => result.status === "fulfilled" ? result.value instanceof BrowserApiError : true;
+		if(results.find(wasErrorOccuredDuringDispatchingNotifications)) return new SendWasntSuccessfulError(this, "Notifiaction wasnt send successful to all tabs", {results});
+		return true;
+	}
+	
+	private async dispatchNotification(tab: BrowserTab, variant: Variants<SN>, data: unknown) : Promise<boolean | TabsApiCallError>
+	{
+		if(isUndefined(tab.id)) return false;
+		const packet = CommProtocol.Pack(variant, data);
+		const returnError = (reason: unknown) => new TabsApiCallError(this,  "Internal browser call `tabs.sendMessage()` throw error.", {tab, packet}, reason);
+		const result = await (browser.tabs.sendMessage(tab.id, packet).catch(returnError)) as unknown; // TODO instead of local use of casting, override method type declaration return value
+		// TODO notification can return data, should we read it?
+		if(isError(TabsApiCallError, result)) return false;
+		return true;
 	}
 	
 	private dispatchMessage(rawPacket: unknown, sender: MessageSender, sendResponse: SendResponse)

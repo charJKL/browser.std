63502eda3f9bbeaab89458bbbc4a0e2f9df167aa
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-28 13:07:55 +0100

    export `AddonScriptApiMethod`, and `AddonScriptApiNotiication`.

--- src/backend.ts
+++ src/backend.ts
@@ -3,4 +3,5 @@ export { BackendComm } from "./api/backend/BackendComm";
 export { NetRequestBlock } from "./api/backend/NetRequestBlock";
 export { Storage } from "./api/backend/Storage";
 
+export type { AddonScriptApiMethod, AddonScriptApiNotification } from "./api/CommProtocol";
 export type { InstaltionDetails, UpdateDetails } from "./api/backend/AddonLifecycle";

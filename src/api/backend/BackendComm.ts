6a0efb9b70886cc0e2ee512ff8b7ffd91498e7ce
charJKL <11600464+charJKL@users.noreply.github.com>
2024-10-29 18:50:45 +0100

    Refactor `BackendComm`.

--- src/api/backend/BackendComm.ts
+++ src/api/backend/BackendComm.ts
@@ -1,89 +1,89 @@
-import { CommProtocol, CorruptedPacketError, type Data, type Packet } from "src/api/CommProtocol";
-import type { MessageBlueprintArgs, MessageBlueprint, SupportedMessages, MessageBlueprintResponse } from "src/api/CommProtocol";
-import type { NotificationData, SupportedNotifications } from "src/api/CommProtocol";
-import type { MessageSender, SendResponse, Variants } from "src/api/CommProtocol";
+import { CommProtocol, CorruptedPacketError } from "src/api/CommProtocol";
+import type { MessageSender, SendResponse, Data, Variants, Packet, ProtocolBlueprint, ProtocolBlueprintArgs, ProtocolBlueprintResponse, SupportedProtocol,  } from "src/api/CommProtocol";
 import { BrowserApiError } from "src/api/BrowserApiError";
-import { ArrayEx, safeCast, isError, isUndefined, isNotArray } from "src/ex";
 import { ApiReturn } from "src/api/ApiReturn.type";
+import { ArrayEx, safeCast, isError, isNotUndefined, isUndefined, isNotArray } from "src/ex";
 
 type BrowserTab = browser.tabs.Tab;
+type ValidBrowserTab = BrowserTab & { id: number };
 type AllowListenerBeAsync<T> = Promise<T> | T;
-type MessageListenerArgs<B extends MessageBlueprint> = [...MessageBlueprintArgs<B>, sender: MessageSender];
-type MessageListenerResponse<B extends MessageBlueprint> = AllowListenerBeAsync<MessageBlueprintResponse<B>>;
-type MessageListener<B extends MessageBlueprint> = (...args: MessageListenerArgs<B>) => MessageListenerResponse<B>;
+type MessageListenerArgs<B extends ProtocolBlueprint> = [...ProtocolBlueprintArgs<B>, sender: MessageSender];
+type MessageListenerResponse<B extends ProtocolBlueprint> = AllowListenerBeAsync<ProtocolBlueprintResponse<B>>;
+type MessageListener<B extends ProtocolBlueprint> = (...args: MessageListenerArgs<B>) => MessageListenerResponse<B>;
+type OptionalProtocolBlueprintArgs<B extends ProtocolBlueprint> = ProtocolBlueprintArgs<B> extends [] ? undefined : ProtocolBlueprintArgs<B>;
 
-// errors:
-class TabsApiCallError extends BrowserApiError<"TabsApiCallError",  BackendComm<any, any>, {tabUrl: string} | {tab: BrowserTab, packet: Packet}>{ };
-class NoTabsWasFound extends BrowserApiError<"NoTabsWasFound", BackendComm<any, any>, {url: string}>{ };
-class SendWasntSuccessfulError extends BrowserApiError<"SendWasntSuccessfulError", BackendComm<any, any>, {results: unknown[]}>{ };
-class InvalidPacketError extends BrowserApiError<"InvalidPacketDescError", BackendComm<any, any>, {payload: unknown, sender: MessageSender}>{ }; // TODO what to do with any? 
-class MissingListenerError extends BrowserApiError<"MissingListnerError", BackendComm<any, any>, {packet: Packet, sender: MessageSender}>{ }; // TODO what to do with any?
+// errors
+class TabsApiCallError extends BrowserApiError<"TabsApiCallError",  BackendComm<any>, {url: string} | {tab: BrowserTab, packet: Packet}>{ };
+class NoTabsWasFound extends BrowserApiError<"NoTabsWasFound", BackendComm<any>, {url: string}>{ };
+class SendWasntSuccessfulError extends BrowserApiError<"SendWasntSuccessfulError", BackendComm<any>, {results: unknown[]}>{ };
+class CorruptedPacketDataError extends BrowserApiError<"CorruptedPacketDataError", BackendComm<any>, {packet: Packet}>{ };
+class NoListenerPresent extends BrowserApiError<"NoListenerPresent", BackendComm<any>, {packet: Packet}>{ };
 
 /**
  * BackendComm
  */
-export class BackendComm<SM extends SupportedMessages, SN extends SupportedNotifications>
+export class BackendComm<SP extends SupportedProtocol>
 {
-	private readonly $listeners: Map<Variants<SM>, MessageListener<MessageBlueprint>>
+	private readonly $listeners: Map<Variants<SP>, MessageListener<ProtocolBlueprint>>;
 	
-	public constructor()
+	constructor()
 	{
 		browser.runtime.onMessage.addListener(this.dispatchMessage.bind(this));
 		this.$listeners = new Map();
 	}
 	
-	public addMessageListener<V extends Variants<SM>>(variant: V, listeners: MessageListener<SM[V]>) : void
+	public addMessageListener<V extends Variants<SP>>(variant: V, listener: MessageListener<SP[V]>) : void
 	{
-		this.$listeners.set(variant, safeCast<MessageListener<SM[V]>, MessageListener<MessageBlueprint>>(listeners));
+		this.$listeners.set(variant, safeCast<MessageListener<SP[V]>, MessageListener<ProtocolBlueprint>>(listener));
 	}
 	
-	public async sendNotification<V extends Variants<SN>>(tabUrl: string, variant: V, data: NotificationData<SN[V]>) : ApiReturn<boolean, TabsApiCallError, NoTabsWasFound, SendWasntSuccessfulError>
+	public async sendMessage<V extends Variants<SP>>(variant: V, url: string, data: OptionalProtocolBlueprintArgs<SP[V]>) : ApiReturn<boolean, TabsApiCallError, NoTabsWasFound, SendWasntSuccessfulError>
 	{
-		console.log("Send notification", "tabUrl=", tabUrl, "variant=", variant, "args=", data); // TODO delete this, for debug purpose only
-		const returnError = (reason: unknown) => new TabsApiCallError(this,  "Internal browser call `tabs.query()` throw error.", {tabUrl}, reason);
-		const tabs = await browser.tabs.query({url: tabUrl}).catch(returnError); // TODO should `BackendComm` directly use `browser.tabs`? or had to inject `BrowserTabs`?
+		console.log("BackendComm.sendMessage()", "variant=", variant, "url=", url, "data=", data); // TODO debug only
+		const returnError = (reason: unknown) => new TabsApiCallError(this,  "Internal browser call `tabs.query()` throw error.", {url}, reason);
+		const tabs = await browser.tabs.query({url}).catch(returnError);
 		if(isError(TabsApiCallError, tabs)) return tabs;
-		if(ArrayEx.IsEmpty(tabs)) return new NoTabsWasFound(this, "Wanted url is not opened in any tab.", {url: tabUrl});
-		const results = await ArrayEx.AsyncMap(tabs, tab => this.dispatchNotification.call(this, tab, variant, data));
-		const wasErrorOccuredDuringDispatchingNotifications = (result: PromiseSettledResult<boolean | TabsApiCallError>) => result.status === "fulfilled" ? result.value instanceof BrowserApiError : true;
+		if(ArrayEx.IsEmpty(tabs)) return new NoTabsWasFound(this, "Wanted url is not opened in any tab.", {url});
+		const validTabs = tabs.filter((tab: BrowserTab) : tab is ValidBrowserTab => isNotUndefined(tab.id));
+		const results = await ArrayEx.AsyncMap(validTabs, (tab) => this.sendMessageToTab.call(this, tab, variant, data));
+		const wasErrorOccuredDuringDispatchingNotifications = (result: PromiseSettledResult<"Sended" | TabsApiCallError>) => result.status === "fulfilled" ? result.value instanceof TabsApiCallError : true;
 		if(results.find(wasErrorOccuredDuringDispatchingNotifications)) return new SendWasntSuccessfulError(this, "Notifiaction wasnt send successful to all tabs", {results});
 		return true;
 	}
 	
-	private async dispatchNotification(tab: BrowserTab, variant: Variants<SN>, data: Data) : Promise<boolean | TabsApiCallError>
+	// TODO should response from content script be discarded?
+	public async sendMessageToTab(tab: ValidBrowserTab, variant: Variants<SP>, data?: Data) : Promise<"Sended" | TabsApiCallError>
 	{
-		if(isUndefined(tab.id)) return false;
-		const packet = CommProtocol.Pack(variant, data);
+		const assuredData = isUndefined(data) ? [] : data;
+		const packet = CommProtocol.Pack(variant, assuredData);
 		const returnError = (reason: unknown) => new TabsApiCallError(this,  "Internal browser call `tabs.sendMessage()` throw error.", {tab, packet}, reason);
-		const result = await (browser.tabs.sendMessage(tab.id, packet).catch(returnError)) as unknown; // TODO instead of local use of casting, override method type declaration return value
-		// TODO notification can return data, should we read it?
-		if(isError(TabsApiCallError, result)) return false;
+		const result = await browser.tabs.sendMessage(tab.id, packet).catch(returnError) as Promise<unknown> | TabsApiCallError; // result is discarded // TODO remove casting 
+		if(isError(TabsApiCallError, result)) return result;
+		return "Sended";
+	}
+	
+	private dispatchMessage(payload: unknown, sender: MessageSender, sendResponse: SendResponse) : boolean
+	{
+		// TODO being silent about error is good choice? Maybe some abstract error should be returned?
+		console.log("BackendComm.dispatchMessage()", "payload=", payload, "sender=", sender); // TODO for debug only
+		void this.dispatchMessageToListener(payload, sender).then((result) => {
+			if(isError(CorruptedPacketError, result)) return;
+			if(isError(CorruptedPacketDataError, result)) return;
+			if(isError(NoListenerPresent, result)) return;
+			const {packet, result: response} = result;
+			sendResponse(CommProtocol.Pack(packet.variant, response));
+		});
 		return true;
 	}
 	
-	private dispatchMessage(payload: unknown, sender: MessageSender, sendResponse: SendResponse)
+	private async dispatchMessageToListener(payload: unknown, sender: MessageSender) : Promise<{packet: Packet, result: Data} | CorruptedPacketError | CorruptedPacketDataError | NoListenerPresent>
 	{
-		try
-		{
-			const packet = CommProtocol.ValidatePacket(payload);
-			if(isError(CorruptedPacketError, packet)) throw new InvalidPacketError(this, "Message packet is invalid.", {payload, sender});
-			const listener = this.$listeners.get(packet.variant);
-			if(isUndefined(listener)) throw new MissingListenerError(this, "Listener for packet is not set.", {packet, sender});
-			if(isNotArray(packet.data)) throw new Error(); // TODO
-			const args = [...packet.data, sender] as const;
-			void (async function() {
-				const result = await listener(...args);
-				const response = CommProtocol.Pack(packet.variant, result);
-				sendResponse(response);
-			})();
-			return true;
-			// Does this need try? SendREsponse
-		}
-		catch(e)
-		{
-			// DO NOTHING
-			console.log(e); // TODO what to do now? Response with failure message?
-			return true;
-		}
+		const packet = CommProtocol.ValidatePacket(payload);
+		if(isError(CorruptedPacketError, packet)) return packet;
+		if(isNotArray(packet.data)) return new CorruptedPacketDataError(this, "Data sended to background script is corrupted.", {packet});
+		const listener = this.$listeners.get(packet.variant);
+		if(isUndefined(listener)) return new NoListenerPresent(this, "There isn't any listener for this message", {packet});
+		const result = await listener(...packet.data, sender);
+		return {packet, result};
 	}
 }

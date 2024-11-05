b9f9a30e2d8ac59627345def4341fdd8bbf92116
charJKL <11600464+charJKL@users.noreply.github.com>
2024-11-05 15:24:42 +0100

    Make eslint ignore more broad.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
 	{
-		ignores: ["dist/**", "eslint.config.mjs"]
+		ignores: ["dist/**", "*.config.mjs"]
 	},
 	eslint.configs.recommended,
 	...tseslint.configs.strictTypeChecked,

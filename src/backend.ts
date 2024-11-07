fd59da951d56f38ea9a016cf4d88c332229edb3c
charJKL <11600464+charJKL@users.noreply.github.com>
2024-11-07 11:58:18 +0100

    Fix bug with wrongly exported helper types.

--- src/backend.ts
+++ src/backend.ts
@@ -4,4 +4,4 @@ export { NetRequestBlock } from "@src/api/backend/NetRequestBlock";
 export { LocalStorage } from "@src/api/backend/LocalStorage";
 
 export type { InstallationDetails, UpdateDetails } from "@src/api/Api";
-export type { AddonScriptApiMethod, AddonScriptApiNotification } from "@src/api/CommProtocol";
+export type { MessageToBackend, MessageToFrontend } from "@src/api/CommProtocol";

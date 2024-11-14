cfbd1aabfef7394d77a7f65d9dd7b2e9b4af6080
charJKL <11600464+charJKL@users.noreply.github.com>
2024-11-14 14:58:38 +0100

    Implement `LocalStorage` tests.

---
+++ tests/LocalStorage.test.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/unbound-method -- global.browser.storage.local.get method is unbund */ 
+import { LocalStorage as ApiLocalStorage } from "@src/api/backend/LocalStorage";
+import { getBrowserMock } from "@src/util/tests/BrowserMock";
+
+beforeAll(function mockBrowserNativeAPIObject()
+{
+	global.browser = getBrowserMock();
+	// TODO those test are not perfect, better mock for `browser.storage` is needed.
+});
+
+beforeEach(() => {
+	jest.clearAllMocks();
+});
+
+test("Using `Map<K,V>` with `api/LocalStorage` should be allowed", async() =>
+{
+	global.browser.storage.local.get = jest.fn().mockImplementation((key: any) => Promise.resolve(key));
+	
+	const blueprint = 
+	{
+		list: new Map()
+	}
+	const localStorage = new ApiLocalStorage(blueprint);
+	
+	const result = await localStorage.get("list");
+	
+	expect(global.browser.storage.local.get).toHaveBeenCalledWith({list: blueprint.list});
+	expect(result).toEqual(new Map());
+});
+
+test("Store `Map<,>` should be allowed, and `Map<,>` should be serialized", async() => 
+{
+	global.browser.storage.local.set = jest.fn().mockImplementation((key: any) => Promise.resolve(key));
+	
+	const blueprint = 
+	{
+		list: new Map()
+	}
+	const localStorage= new ApiLocalStorage(blueprint);
+	const value = new Map([[1, "one"], [2, "two"]]);
+	const result = await localStorage.save("list", value);
+	
+	expect(global.browser.storage.local.set).toHaveBeenCalledWith({"list": {__map__: true, entries: [{key: 1, value: "one"},{key: 2, value: "two"},]}});
+	expect(result).toBe(value);
+})

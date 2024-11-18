29e2b84c34be175e6e5b99d9376b2b78dfd41b53
charJKL <11600464+charJKL@users.noreply.github.com>
2024-11-18 08:05:19 +0100

    Change export type from `backend`.

--- src/backend.ts
+++ src/backend.ts
@@ -3,5 +3,5 @@ export { BackendComm } from "@src/api/backend/BackendComm";
 export { NetRequestBlock } from "@src/api/backend/NetRequestBlock";
 export { LocalStorage } from "@src/api/backend/LocalStorage";
 
-export type { BrowserNativeApiCallError, InstallationDetails, UpdateDetails } from "@src/api/Api";
-export type { MessageToBackend, MessageToFrontend } from "@src/api/CommProtocol";
+export { BrowserNativeApiCallError, InstallationDetails, UpdateDetails } from "@src/api/Api";
+export { MessageToBackend, MessageToFrontend } from "@src/api/CommProtocol";
